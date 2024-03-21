b25ba850960e4e8e63b5c110bd4ea3d3c5b81d9e
jeronimo <jerov79@gmail.com>
2024-03-21 12:29:26 -0500

    sign in contratista html y css

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def viaje():
-    return render_template('home.html')
+    return render_template('sign_in_contratista.html')
 
 
 if __name__ == '__main__':
