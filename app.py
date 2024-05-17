e8e177c8eba3d25945981683cebe429934d7aee6
jeronimo <jerov79@gmail.com>
2024-05-16 21:14:09 -0500

    contratista vista 1

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def viaje():
-    return render_template('loginContratista.html')
+    return render_template('ContratistaV1.html')
 
 
 if __name__ == '__main__':
