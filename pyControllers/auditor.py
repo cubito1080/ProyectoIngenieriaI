e46b31f4813589b6f0afad928d101e033e2e5705
07santiago21 <santiagolopezramirez0721@gmail.com>
2024-05-20 17:16:33 -0500

    .

--- pyControllers/auditor.py
+++ pyControllers/auditor.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, Blueprint, send_from_directory
+import mysql.connector
+from flask import Flask, render_template, request, Blueprint, send_from_directory, redirect, url_for
 from dbConnection import connectDB
 from documento import documentosContrato
 
@@ -7,22 +8,60 @@ app = Flask(__name__, static_folder='../static', template_folder='../templates')
 auditor_blueprint = Blueprint('auditor', __name__)
 
 
-@auditor_blueprint.route('/auditorV4', methods=['POST'])
-def auditorV4():
-    nombre = request.form.get('nombre_input')
-    tipo_contrato = request.form.get('tipo_contrato_input')
-    clase_arl = request.form.get('clase_arl_input')
-    cedula = request.form.get('cedula_input')
-    metodo_de_pago = request.form.get('metodo_de_pago_input')
-    fecha = request.form.get('fecha_input')
-    sueldo = request.form.get('sueldo_input')
-    bancaria = request.form.get('bancaria_input')
-    fecha_fin = request.form.get('fecha_fin_input')
-    servicio = request.form.get('servicio_input')
-    return render_template('auditorV4.html')
 
 
-@auditor_blueprint.route('/AuditorV3/<cedula>/<cedulaContratista>')
+
+@auditor_blueprint.route('/auditorV4/<nombre>/<cedula>',methods=['GET', 'POST'])
+def auditorV4(nombre, cedula):
+    if request.method == 'POST':
+
+        nombre_ = request.form.get('nombre_')
+        tipo_contrato = request.form.get('tipo_contrato')
+        clase_arl = request.form.get('clase_arl')
+        cedula_contratista = request.form.get('cedula_')
+        metodo_de_pago = request.form.get('metodo_de_pago')
+        fecha = request.form.get('fecha')
+        sueldo = request.form.get('sueldo')
+        bancaria = request.form.get('bancaria')
+        fecha_fin = request.form.get('fecha_fin')
+        servicio = request.form.get('servicio')
+
+        try:
+            connection = connectDB()
+
+            cursor = connection.cursor(dictionary=True)
+
+            insert_query = (
+                "INSERT INTO contrato "
+                "(fecha_inicio, fecha_culminacion, estado, cedula_contratista, "
+                "cedula_auditor, clase_ARL, metodo_pago, tipo, descripcion, honorario) "
+                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            )
+
+            # Datos a insertar
+            data = (
+                fecha, fecha_fin, "en progreso", int(cedula_contratista),
+                int(cedula), clase_arl, metodo_de_pago, tipo_contrato, servicio, sueldo
+            )
+            # Ejecuta la consulta
+            cursor.execute(insert_query, data)
+
+
+            connection.commit()
+            cursor.close()
+
+            return redirect(url_for(f'auditor.auditorV1',
+                                    nombre=nombre, cedula=cedula))
+        except mysql.connector.Error as e:
+            return render_template('qqqq.html', auditor={"nombre": nombre, "cedula":cedula})
+
+    if request.method == 'GET':
+        return render_template('qqqq.html', auditor={"nombre": nombre, "cedula":cedula})
+
+
+
+
+@auditor_blueprint.route('/auditorV3/<cedula>/<cedulaContratista>')
 #Datos QUEMADOS. Debe recibir el contratista y auditor, o la cedula de ellos
 def auditorV3(cedula, cedulaContratista):
 
--- pyControllers/login.py
+++ pyControllers/login.py
@@ -1,3 +1,4 @@
+import mysql.connector
 from flask import Flask, render_template, request
 
 from dbConnection import connectDB
@@ -8,15 +9,28 @@ app = Flask(__name__, static_folder='../static', template_folder='../templates')
 def login():
     if request.method == 'POST':
         #manejar post, al hundirse el boton "registrarse" habra que hacer exepciones y redigir al sign in o home
-        connection = connectDB()
-        request.form.get('email')
-        request.form.get('nombre')
-        request.form.get('password')
-        request.form.get('apellido')
-        request.form.get('cedula')
-        request.form.get('telefono')
 
-        #Crear el contratista, (cerrar el cursor si se abrió)
+        email = request.form.get('email')
+        nombre = request.form.get('nombre')
+        password = request.form.get('password')
+        apellido = request.form.get('apellido')
+        cedula = int(request.form.get('cedula'))
+        telefono = request.form.get('telefono')
+
+        try:
+            connection = connectDB()
+
+            cursor = connection.cursor(dictionary=True)
+
+            # Completar la consulta SQL para insertar los datos en la tabla "contratista"
+            query = "INSERT INTO contratista (email, nombre, contraseña, apellido, cedula, telefono) VALUES (%s, %s, %s, %s, %s, %s)"
+            cursor.execute(query, (email, nombre, password, apellido, cedula, telefono))
+            connection.commit()
+            cursor.close()
+            return render_template('home.html')
+
+        except mysql.connector.Error as e:
+            return render_template("logInContratista.html")
 
 
     if request.method == 'GET':
